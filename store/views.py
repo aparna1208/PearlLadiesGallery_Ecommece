0e170f1e508f5167580b945cb339c5dc32af8b00
aparna1208 <aparna4979@gmail.com>
2025-11-18 20:46:44 +0530

    final Update

--- store/views.py
+++ store/views.py
@@ -99,7 +99,7 @@ def submit_review(request, product_id):
     if request.method == "POST":
         try:
             reviews = ReviewRating.objects.get(user__id = request.user.id, product__id=product_id)
-            form = ReviewForm(request.POST, Instance=reviews)
+            form = ReviewForm(request.POST, instance=reviews)
             form.save()
             messages.success(request,"Thank you! Your review has been updated.")
             return redirect(url)
